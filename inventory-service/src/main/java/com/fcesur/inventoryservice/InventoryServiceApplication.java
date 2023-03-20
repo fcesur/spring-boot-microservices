43d6e9765b555a4b7420db67613bba86ca2bd68c
Fatih Cesur <fc.fatihcesur@gmail.com>
2023-03-21 00:36:02 +0300

    Inventory Service and Order Service projects created.

---
+++ inventory-service/src/main/java/com/fcesur/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,11 @@
+package com.fcesur.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+}
